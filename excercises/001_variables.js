85dec88b3f5400344ca5791ba1d737b76eb2cb72
x-sss-x <manu48617@gmail.com>
2026-07-29 10:06:43 +0530

    feat: add excercise 002_conditional_statements

--- excercises/001_variables.js
+++ excercises/001_variables.js
@@ -1,6 +1,10 @@
+
 // Find out square of given number and store it in `sqr` const
 
 const num = 4;
 const sqrt = ???; // Do something here
 
 console.log(`Sqr of ${num} is ${sqrt}`);
+
+
+
---
+++ excercises/002_conditional_statements.js
@@ -0,0 +1,16 @@
+/** Find out number is odd or even
+ *
+ * Take number from the user and convert that to number.
+ * Print given number is odd or even
+ * */
+
+const num = null;
+
+// Take input from the user & verify if it's valid number
+
+// Print number is odd / even
+
+
+// Put expression to find out the odd or even
+if (???) console.log("This number is odd");
+else console.log("This number is even");
