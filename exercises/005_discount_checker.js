fb2d15fa406a9adfd8a1ca102deea06fc1c854ed
x-sss-x <manu48617@gmail.com>
2026-07-29 16:22:27 +0530

    feat: solution for 005_discount_check.js

--- exercises/005_discount_checker.js
+++ exercises/005_discount_checker.js
@@ -32,7 +32,32 @@ Discount Applied
 */
 
 // TODO: Ask for purchase amount
+const amount = Number(prompt("Enter your purchase amount:")); // "2000" -> 2000
 
-// TODO: Convert to Number
+if (isNaN(amount)) {
+  alert("Invalid amount");
+} else {
+  const discount = 0.1;
 
-// TODO: Write the condition
+  const gst = 0.18;
+
+  const hasDiscountApplied = amount >= 1000 ? true : false;
+
+  const isTaxPayer = confirm("Are you a tax payer citizen?");
+
+  const discountAmount = hasDiscountApplied ? amount * discount : 0;
+
+  const totalAmount = amount - discountAmount;
+
+  const taxAmount = isTaxPayer ? totalAmount * gst : 0;
+
+  const netTotalAmount = isTaxPayer ? totalAmount + taxAmount : totalAmount;
+
+  alert(`
+    Your amount: ${amount}\n
+    Your discount: ${discountAmount}\n 
+    Your TotalAmount is = ${totalAmount}\n
+    Tax = ${taxAmount}\n
+    Your net total amount = ${netTotalAmount}\n
+`);
+}
